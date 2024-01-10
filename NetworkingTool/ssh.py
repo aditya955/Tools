e24ad520d43e519875b7b924c2a644247b5c2881
Aditya <adityajha2103@gmail.com>
2024-01-10 13:58:14 +0530

    Updated ssh.py file for both client and server

--- NetworkingTool/ssh.py
+++ NetworkingTool/ssh.py
@@ -1,91 +1,176 @@
-# Common for client and server
-import paramiko
+#!/bin/env python3
 
-# For Client
-import shlex
-import subprocess
-
-# For Server
-import os
-import socket
+# Server
 import sys
+import socket
 import threading
 
-def run_command(ip, port, user, passwd, cmd):
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Client
+import subprocess
 
-    try:
-        client.connect(ip, port=port, username=user, password=passwd)
-    except paramiko.ssh_exception.AuthenticationException:
-        print("[-] Invalid Credentials")
-        return
-    except paramiko.ssh_exception.NoValidConnectionsError:
-        print("[-] Connection Failed")
-        return
+# Common for server and clinet
+import paramiko
 
-    _, stdout, stderr = client.exec_command(cmd)
-    output = stdout.readlines() + stderr.readlines()
-    if output:
-        print("\n--- Connection to " + ip + ":" + str(port) + " established successfully ---")
-        print("$ " + cmd)
-        for line in output:
-            print(line.strip())
-    
-def client(ip, port, user, passwd, cmd="ClientConnected"):
+USERNAME = "admin"
+PASSWORD = "verySecure"
+
+
+def ssh_command(ip, user, passwd, command):
+    '''
+    ssh_command(ip, user, passwd, command): Connects to a remote host and executes a command
+        ip: IP address of the remote host
+        user: Username to use for authentication
+        passwd: Password to use for authentication
+        command: Command to execute on the remote host
+
+    This function can also be used to connect to regular SSH servers.
+    '''
     client = paramiko.SSHClient()
+    # client.load_host_keys('~/.ssh/known_hosts')
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip, port=port, username=user, password=passwd)
+    client.connect(ip, username=user, password=passwd)
     ssh_session = client.get_transport().open_session()
-
     if ssh_session.active:
-        ssh_session.send(cmd)
-        print(ssh_session.recv(1024).decode())
-        while True:
-            cmd = ssh_session.recv(1024)
-            try:
-                cmd_output = cmd.decode()
-                if(cmd_output == 'exit'):
-                    client.close()
-                    break
+        ssh_session.exec_command(command)
+        result = ssh_session.recv(1024)
 
-                cmd_output = subprocess.check_output(shlex.split(cmd), shell=True)
-                ssh_session.send(cmd_output or "Command not found")
-            except Exception as e:
-                ssh_session.send(str(e))
-        client.close()
+        try:
+            print(result.decode('utf-8'))
+        except UnicodeDecodeError:
+            print(result)
+    return
 
-def server():
-    CWD = os.path.dirname(os.path.realpath(__file__))
-    HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'id_rsa'))
 
-    class Server(paramiko.ServerInterface):
-        def __init__(self) -> None:
+def server(server, port):
+    '''
+    server(server, port): Creates a SSH server
+        server: IP address of the server
+        port: Port to listen on
+
+    This function can be used to create a SSH server that can be connected to using the client function.
+    '''
+    host_key = paramiko.RSAKey(filename="test_rsa.key")
+
+    class Server (paramiko.ServerInterface):
+        def __init__(self):
             self.event = threading.Event()
 
-        def check_channel_request(self, kind: str, chanid: int) -> int:
-            if kind == "session":
+        def check_channel_request(self, kind: str, chanid: int):
+            if (kind == "session"):
                 return paramiko.OPEN_SUCCEEDED
             return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+        # return super().check_channel_request(kind, chanid)
 
-        def check_auth_password(self, username: str, password: str) -> int:
-            if(username == "aditya" and password == "password"):
+        def check_auth_password(self, username, password):
+            print("[+] Checking authentication...")
+            if (username == USERNAME and password == PASSWORD):
                 return paramiko.AUTH_SUCCESSFUL
-        
+            return paramiko.AUTH_FAILED
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((server, port))
+        sock.listen(100)
+        print("[+] Listening for connection...")
+        client, addr = sock.accept()
+    except Exception as e:
+        print("[-] Listen failed:", str(e))
+        sys.exit(1)
+    print("[+] Got connection...")
+
+    try:
+        session = paramiko.Transport(client)
+        session.add_server_key(host_key)
+        server = Server()
+        try:
+            session.start_server(server=server)
+        except paramiko.SSHException as x:
+            print("[-] SSH negotiation failed...")
+        channel = session.accept(20)
+        print("[+] Authenticated...")
+        print(channel.recv(1024))
+        channel.send("Welcome to SSH")
+
+        while True:
+            try:
+                command = input("Enter command: ").strip("\n")
+
+                channel.send(command)
+
+                if (command == "exit"):
+                    print("Exiting...")
+                    sys.exit(0)
+
+                result = channel.recv(1024)
+                try:
+                    print(result.decode('utf-8'))
+                except UnicodeDecodeError:
+                    print(result)
+            except KeyboardInterrupt:
+                session.close()
+    except Exception as e:
+        print("[-] Error:", str(e))
+
+        try:
+            session.close()
+        except:
+            pass
+        sys.exit(1)
+
+
+def client(ip, port, user, passwd, command):
+    '''
+    client(ip, port, user, passwd, command): Connects to a SSH server and executes a command
+        ip: IP address of the SSH server
+        port: Port to connect to
+        user: Username to use for authentication
+        passwd: Password to use for authentication
+        command: Command to execute on the SSH server (for first time while connecting)
+
+    This function can be used to connect to the SSH server created by the server function.
+    '''
+    print(f"[+] Connecting to {ip}:{port}...")
+    client = paramiko.SSHClient()
+    # client.load_host_keys('/home/justin/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd, port=port)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+
+        # Read banner
+        print(ssh_session.recv(1024).decode('utf-8'))
+
+        try:
+            while True:
+                # Get command from SSH server
+                command = ssh_session.recv(1024)
+                if (command == "exit"):
+                    break
+
+                try:
+                    cmd_output = subprocess.check_output(
+                        command.decode('utf-8'), shell=True)
+                    ssh_session.send(cmd_output)
+                except Exception as e:
+                    ssh_session.send(str(e))
+            client.close()
+        except OSError:
+            print("[+] Connection closed...")
+    return
 
 
 if __name__ == '__main__':
-    import getpass
-    default_user = getpass.getuser()
-    user = input("Enter Username (Default = " + default_user + "): ")
-    passwd = getpass.getpass("Password: ")
-    ip = input("Enter Server IP (Default = 127.0.0.1): ")
-    port = input("Enter Port (Default = 22): ")
-    cmd = input("Enter Command (Default = whoami): ")
+    # import getpass
+    # ssh_command("192.168.0.3", input("Enter username: "), getpass.getpass(
+    # "Enter password: "), input("Enter command: "))
 
-    user = user if user else default_user
-    ip = ip if ip else "127.0.0.1"
-    port = port if port else 22
-    cmd = cmd if cmd else "whoami"
+    run_as = input("Run as ((s)erver / (c)lient): ").lower()
 
-    run_command(ip, port, user, passwd, cmd)
+    SERVER_IP = "192.168.0.3"
+    PORT = 4444
+    if (run_as == 's'):
+        server(SERVER_IP, PORT)
+    else:
+        client(SERVER_IP, PORT, USERNAME, PASSWORD, "ClientConnected")
